9a3c35d08792098435a13d958e2c705c2d37d571
Philip Yong <pyong@littlefieldagency.com>
2024-04-11 18:23:39 -0500

    Create Pokemon with Form and POST Request, for step 10

--- index.js
+++ index.js
@@ -14,7 +14,9 @@ const PRIVATE_APP_ACCESS = process.env.ACCESS_TOKEN;
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
-app.get("/", async (req, res) => {});
+app.get("/", async (req, res) => {
+  res.send("Hello World!");
+});
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
@@ -26,7 +28,33 @@ app.get("/update-cobj", async (req, res) => {
 
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
 
-app.post("/update-cobj", async (req, res) => {});
+app.post("/update-cobj", async (req, res) => {
+  let pokemon_type = req.body.pokemon_type;
+  if (Array.isArray(pokemon_type)) {
+    pokemon_type = pokemon_type.join(";");
+  }
+
+  const create = {
+    properties: {
+      name: req.body.name,
+      pokedex_id: req.body.pokedex_id,
+      pokemon_type: pokemon_type,
+    },
+  };
+
+  const createPokemon = `https://api.hubapi.com/crm/v3/objects/pokemons`;
+  const headers = {
+    Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+    "Content-Type": "application/json",
+  };
+
+  try {
+    await axios.post(createPokemon, create, { headers });
+    res.redirect("/");
+  } catch (err) {
+    console.error(err);
+  }
+});
 
 /** 
 * * This is sample code to give you a reference for how you should structure your calls. 
