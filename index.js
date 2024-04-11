0fed5231bf85b00258edbd55e49f00bf24640642
Philip Yong <pyong@littlefieldagency.com>
2024-04-11 17:52:21 -0500

    Fix routes and add updates template to step 9

--- index.js
+++ index.js
@@ -18,11 +18,15 @@ app.get("/", async (req, res) => {});
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
-app.get("update-cobj", async (req, res) => {});
+app.get("/update-cobj", async (req, res) => {
+  res.render("updates", {
+    title: "Update Custom Object Form | Integrating With HubSpot I Practicum",
+  });
+});
 
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
 
-app.post("update-cobj", async (req, res) => {});
+app.post("/update-cobj", async (req, res) => {});
 
 /** 
 * * This is sample code to give you a reference for how you should structure your calls. 
